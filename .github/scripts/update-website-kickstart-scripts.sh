42ed6deaa36eda4b8d693986e8d6ae931a05020e
James Mills <prologic@shortcircuit.net.au>
2020-03-10 09:39:33 +1000

    Add automation to update website kickstart* scripts (#14)
    * Add automation to update website kickstart* scripts
    
    * Add -ex to bail out on firs terror and verify what we're actually running
    
    * Trigger and test automation in PR

--- .github/scripts/update-website-kickstart-scripts.sh
+++ .github/scripts/update-website-kickstart-scripts.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-ssh -p 2222 root@138.68.182.52 /bin/sh << EOF
+ssh -p 2222 root@138.68.182.52 /bin/sh -ex << EOF
 cd /usr/src/netdata.git
 git pull -q
 cat packaging/installer/kickstart.sh > /usr/share/netdata/web/kickstart.sh
