84fafae7619c7bd037086c4390dad177452bb7c4
James Mills <prologic@shortcircuit.net.au>
2020-03-09 14:54:12 +1000

    Add script to update website kickstart* scripts (#10)

---
+++ prologic/update-website-kickstart-scripts.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+ssh -p 2222 root@138.68.182.52 /bin/sh << EOF
+cd /usr/src/netdata.git
+git pull -q
+cat packaging/installer/kickstart.sh > /usr/share/netdata/web/kickstart.sh
+cat packaging/installer/kickstart-static64.sh > /usr/share/netdata/web/kickstart-static64.sh
+EOF
