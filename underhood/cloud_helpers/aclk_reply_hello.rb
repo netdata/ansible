497337be74580bd85f8751f8e81b8186b8544ff4
Timotej Siskovic <timotej@netdata.cloud>
2020-08-28 10:43:48 +0200

    add option to send also v2 query for convinience

--- underhood/cloud_helpers/aclk_reply_hello.rb
+++ underhood/cloud_helpers/aclk_reply_hello.rb
@@ -9,15 +9,26 @@ $verbose = false
 $mqtt_host = '127.0.0.1'
 $mqtt_port = 1883
 
+$req_v2 = <<-REQUEST
+{
+  "type": "http",
+  "version": 2,
+  "callback-topic": "/svc/agent-data-ctrl/callback",
+  "msg-id": "afb69b30-cb20-11ea-bb71-3085a98d4bee/c53d9aa6-e2cf-4d95-bb63-d491d04ffafc/data/1596042321983669351",
+}\x0D\x0A\x0D\x0AGET /api/v1/data?chart=system.cpu&format=json&points=420&group=average&after=-300 HTTP/1.1\x0D\x0AAccept: application/json, text/plain, */*\x0D\x0AAccept-Encoding: gzip, deflate, br\x0D\x0A
+REQUEST
+
 def usage
     STDERR.puts "Usage: \"aclk_reply_hello.rb [min_aclk_version] [max_aclk_version]\""
 end
 
-if ARGV.size != 2
+if ARGV.size < 2
     STDERR.puts "2 arguments needed min and max version of protocol to pretend supporting".red
     usage
     exit 1
 end
+
+$send_v2_request = ARGV.size == 3 && ARGV[2].downcase == 'true'
 
 $min_ver = ARGV[0].to_i
 $max_ver = ARGV[1].to_i
@@ -41,6 +52,7 @@ rescue Errno::ECONNREFUSED => e
   exit 4
 end
 STDERR.puts "Listening for \"hello\" message and replying with min, max = #{$min_ver}, #{$max_ver}".green
+STDERR.puts "Will #{"NOT (default) " if !$send_v2_request}send v2 (compression) test query after connect."
 
 @client.subscribe("/agent/+/outbound/#")
 begin
@@ -50,11 +62,21 @@ begin
       reply_topic = "/agent/#{topic.split('/')[2]}/inbound/cmd"
       msg = "{\"type\":\"version\",\"version\":#{json[:version]},\"min-version\":#{$min_ver},\"max-version\":#{$max_ver}}"
       STDERR.puts "Got \"hello\" message from agent with min=#{json[:"min-version"]}, max=#{json[:"max-version"]}".yellow
-      STDERR.puts "Sending to \"#{reply_topic}\" reply:".yellow
+      STDERR.puts "Sending to \"#{reply_topic}\" reply:".green
       STDERR.puts JSON.pretty_generate(JSON.parse(msg))
       @client.publish(reply_topic, msg)
+      STDERR.puts "Wait for agent \"connect\" message. It might take some time due to popcorning etc.".green
     elsif json[:type] == 'connect'
       STDERR.puts "Got \"connect\" from agent with version #{json[:version]}".yellow
+      if $send_v2_request
+        if json[:version] >= 2 && $send_v2_request
+          STDERR.puts "Sending V2 test query.".green
+          @client.publish("/agent/#{topic.split('/')[2]}/inbound/cmd", $req_v2)
+        else
+          STDERR.puts "Will not send V2 test query as \"connect\" message received from agent is of too old version".red
+        end
+      end
+      STDERR.puts ">>All Done<< You can restart The AGENT to test again. No need to restart this script (it will continue to listen). CTRL+C to end if you want to e.g. change parameters."
     else
       puts "Got msg type \"#{json[:type]}\" ignoring." if $verbose
     end
