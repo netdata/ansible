e9cceaae4524832547e60dc362bd96a411719d50
Austin S. Hemmelgarn <austin@netdata.cloud>
2022-03-15 14:22:06 -0400

    Overhaul nightly build checks.

--- .github/scripts/update-website-kickstart-scripts.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-ssh -p 2222 root@138.68.182.52 /bin/sh -ex << EOF
-cd /usr/src/netdata.git
-git pull -q
-cat packaging/installer/kickstart.sh > /usr/share/netdata/web/kickstart.sh
-cat packaging/installer/kickstart-static64.sh > /usr/share/netdata/web/kickstart-static64.sh
-EOF
